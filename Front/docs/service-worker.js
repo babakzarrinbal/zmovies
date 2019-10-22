280798a80233eb91c82dd38a8624ff76349bff39
babak <babak.zarrinbal@gmail.com>
2019-10-22 13:05:24 +0330

    added literotica whole story add

---
+++ Front/docs/service-worker.js
@@ -0,0 +1,40 @@
+importScripts("/zmovies/precache-manifest.cdddc450992df92518ac3b6c754eb63d.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+self.addEventListener("install", function(event) {
+  // console.log("Service Worker installing.");
+});
+
+self.addEventListener("activate", function(event) {
+  // console.log("Service Worker activating.");
+});
+self.addEventListener("fetch", function(e) {
+  // console.log("fetched", e.request.url);
+});
+
+self.addEventListener("push", function(event) {
+  var data = event.data.json();
+  const title = data.title || "Driver @ Dilivir";
+  const options = {
+    body: data.body || "New orders",
+    icon: data.icon || "img/icons/logo.png",
+    badge: data.badge || "img/icons/logo.png",
+    data,
+  };
+
+  if (data.tag) options.tag = data.tag;
+  const notificationPromise = self.registration.showNotification(
+    title,
+    options
+  );
+  event.waitUntil(notificationPromise);
+});
+
+self.addEventListener("notificationclick", function(event) {
+  event.notification.close();
+  event.waitUntil(
+    clients.openWindow(
+      "http://localhost:5000/#/" + (event.notification.data || {}).gotourl || ""
+    )
+  );
+});
+
---
+++ Front/src/router.js
@@ -0,0 +1,66 @@
+import Vue from "vue";
+import Router from "vue-router";
+import MainList from "./views/MainList.vue";
+import TTS from "./views/TTS.vue";
+import Drivers from "./views/Drivers.vue";
+
+Vue.use(Router);
+
+var router = new Router({
+  routes: [
+    {
+      path: "/",
+      name: "MainList",
+      component: MainList,
+      meta: { mainindex: 1, keepAlive: true }
+    },
+    {
+      path: "/drivers",
+      name: "Drivers",
+      component: Drivers,
+      meta: { mainindex: 2, keepAlive: true }
+    },
+    {
+      path: "/tts",
+      name: "TTS",
+      component: TTS,
+      meta: { mainindex: 3, keepAlive: true }
+    }
+
+    // {
+    //   path: "/new/:order_id?",
+    //   name: "newOrder",
+    //   component: newOrder,
+    //   meta: { mainindex: 9 },
+    // },
+    // {
+    //   path: "/drivers",
+    //   name: "drivers",
+    //   component: drivers,
+    //   meta: { mainindex: 3 },
+    // },
+    // {
+    //   path: "/login",
+    //   name: "login",
+    //   component: login,
+    // },
+    // {
+    //   path: "/ongoingorders",
+    //   name: "ongoingorders",
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () =>
+    //     import(/* webpackChunkName: "ongoing" */ "./views/About.vue"),
+    // },
+  ]
+});
+
+// router.beforeEach((to, from, next) => {
+//   if (!global.user && from.name != "login" && to.name != "login") {
+//     return next("/login");
+//   }
+//   return next();
+// });
+
+export default router;
